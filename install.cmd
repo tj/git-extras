01c313957c34711f2898ea020b4ed2a84a07d272
Huanming Peng <gisphm@users.noreply.github.com>
2015-10-19 20:53:32 +0800

    Update install.cmd
    Reduce duplicate code in the previous two for loops and copy bash_completion.sh to where it should be installed as the makefile describes.

--- install.cmd
+++ install.cmd
@@ -8,23 +8,17 @@ IF NOT "%~1"=="" SET PREFIX="%~1"
 SET HTMLDIR=%PREFIX%\share\doc\git-doc
 SET GITEXTRAS=%~dp0
 
-IF NOT EXIST %PREFIX%\bin MKDIR %PREFIX%\bin
-
-SET COMMANDS_WITHOUT_REPO=git-alias git-extras git-fork git-setup
+IF NOT EXIST %PREFIX%\etc\bash_completion.d MKDIR %PREFIX%\etc\bash_completion.d
+COPY %GITEXTRAS%\etc\bash_completion.sh	%PREFIX%\etc\bash_completion.d\git-extras
 
 FOR /R %GITEXTRAS%\bin %%i in (*.*) DO (
 	ECHO #!/usr/bin/env bash > %PREFIX%\bin\%%~ni
 	TYPE %GITEXTRAS%\helper\reset-env >> %PREFIX%\bin\%%~ni
 	TYPE %GITEXTRAS%\helper\git-extra-utility >> %PREFIX%\bin\%%~ni
-	TYPE %GITEXTRAS%\helper\is-git-repo >> %PREFIX%\bin\%%~ni
+	>NUL FINDSTR /C:%%~ni %GITEXTRAS%\need_git_repo && (
+		TYPE %GITEXTRAS%\helper\is-git-repo >> %PREFIX%\bin\%%~ni
+	)
 	MORE +2 %GITEXTRAS%\bin\%%~ni >> %PREFIX%\bin\%%~ni
-)
-
-FOR %%i in (%COMMANDS_WITHOUT_REPO%) DO (
-	ECHO #!/usr/bin/env bash > %PREFIX%\bin\%%i
-	TYPE %GITEXTRAS%\helper\reset-env >> %PREFIX%\bin\%%i
-	TYPE %GITEXTRAS%\helper\git-extra-utility >> %PREFIX%\bin\%%i
-	MORE +2 %GITEXTRAS%\bin\%%i >> %PREFIX%\bin\%%i
 )
 
 ROBOCOPY /IS %GITEXTRAS%\man %HTMLDIR% *.html
