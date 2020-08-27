e116406ff12a46465a1d8ef5d79bb871d1944ed4
spacewander <spacewanderlzx@gmail.com>
2020-08-27 11:07:11 +0800

    git-back: removed

--- etc/git-extras-completion.zsh
+++ etc/git-extras-completion.zsh
@@ -332,7 +332,6 @@ zstyle ':completion:*:*:git:*' user-commands $existing_user_commands \
     alias:'define, search and show aliases' \
     archive-file:'export the current head of the git repository to an archive' \
     authors:'generate authors report' \
-    back:'undo and stage latest commits' \
     browse:'open repo website in browser' \
     bug:'create bug branch' \
     bulk:'run bulk commands' \
