a17d2fb15d9d27b7781973f8b51a15ca02b2ccab
Leroy <vanpipy@gmail.com>
2024-02-29 11:11:00 +0800

    refactor: test utils and simplify the case code (#1136)
    * doc(tests/README.md): explain the unit test process
    
    * test(TempRepository): support to change origin and get filename
    
    * refactor: simplify the case code
    
        * test_git_browse_ci.py
        * test_git_browse.py
    
    * fix: rename `test_authors.py` to `test_git_authors.py`
    
    * fix: typo
    
    * fix: log the GitCommandError

--- tests/test_git_browse_ci.py
+++ tests/test_git_browse_ci.py
@@ -1,174 +1,129 @@
 import os, subprocess
 from testpath import MockCommand, modified_env
 
-github_origin = "https://github.com/tj/git-extras"
-gitlab_origin = "https://gitlab.com/tj/git-extras"
-bitbucket_origin = "https://bitbucket.org/tj/git-extras"
-unknown_site_origin = "https://unknown-site.com/tj/git-extras"
+UNKNOWN_SITE_ORIGIN = "https://unknown-site.com/tj/git-extras.git"
 
-def set_origin_url(git, url):
-    git.remote("set-url", "origin", url + ".git")
-
-def create_expected_action_url(git, mode):
+def get_ci_uri_by_domain(mode):
     if mode == "github":
-        return github_origin + '/actions'
+        return "https://github.com/tj/git-extras/actions"
     if mode == "gitlab":
-        return gitlab_origin + '/-/pipelines'
+        return "https://gitlab.com/tj/git-extras/-/pipelines"
     if mode == "bitbucket":
-        return bitbucket_origin + '/addon/pipelines/home'
+        return "https://bitbucket.org/tj/git-extras/addon/pipelines/home"
 
 class TestGitBrowse:
     def test_browse_github_ci_on_mac(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        git.remote("add", "origin", github_origin + ".git")
-        with modified_env({ "OSTYPE": "darwin" }):
-            with MockCommand("open") as openCommand:
-                temp_repo.invoke_extras_command("browse-ci", "origin")
-                expected_url = create_expected_action_url(git, "github")
-                openCommand.assert_called([expected_url])
+        with modified_env({ "OSTYPE": "darwin" }), MockCommand("open") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("github")
+            openCommand.assert_called([expected_url])
 
     def test_browse_gitlab_ci_on_mac(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, gitlab_origin)
-        with modified_env({ "OSTYPE": "darwin" }):
-            with MockCommand("open") as openCommand:
-                temp_repo.invoke_extras_command("browse-ci", "origin")
-                expected_url = create_expected_action_url(git, "gitlab")
-                openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_gitlab()
+        with modified_env({ "OSTYPE": "darwin" }), MockCommand("open") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("gitlab")
+            openCommand.assert_called([expected_url])
 
     def test_browse_bitbucket_ci_on_mac(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, bitbucket_origin)
-        with modified_env({ "OSTYPE": "darwin" }):
-            with MockCommand("open") as openCommand:
-                temp_repo.invoke_extras_command("browse-ci", "origin")
-                expected_url = create_expected_action_url(git, "bitbucket")
-                openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_bitbucket()
+        with modified_env({ "OSTYPE": "darwin" }), MockCommand("open") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("bitbucket")
+            openCommand.assert_called([expected_url])
 
     def test_browse_github_ci_on_git_bash_on_window(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, github_origin)
-        with modified_env({ "OSTYPE": "msys" }):
-            with MockCommand("start") as openCommand:
-                temp_repo.invoke_extras_command("browse-ci", "origin")
-                expected_url = create_expected_action_url(git, "github")
-                openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_github()
+        with modified_env({ "OSTYPE": "msys" }), MockCommand("start") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("github")
+            openCommand.assert_called([expected_url])
 
     def test_browse_gitlab_ci_on_git_bash_on_window(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, gitlab_origin)
-        with modified_env({ "OSTYPE": "msys" }):
-            with MockCommand("start") as openCommand:
-                temp_repo.invoke_extras_command("browse-ci", "origin")
-                expected_url = create_expected_action_url(git, "gitlab")
-                openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_gitlab()
+        with modified_env({ "OSTYPE": "msys" }), MockCommand("start") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("gitlab")
+            openCommand.assert_called([expected_url])
 
     def test_browse_bitbucket_ci_on_git_bash_on_window(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, bitbucket_origin)
-        with modified_env({ "OSTYPE": "msys" }):
-            with MockCommand("start") as openCommand:
-                temp_repo.invoke_extras_command("browse-ci", "origin")
-                expected_url = create_expected_action_url(git, "bitbucket")
-                openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_bitbucket()
+        with modified_env({ "OSTYPE": "msys" }), MockCommand("start") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("bitbucket")
+            openCommand.assert_called([expected_url])
 
     def test_browse_github_ci_on_WSL_with_microsoft_key(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, github_origin)
-        with modified_env({ "OSTYPE": "linux" }):
-            with MockCommand.fixed_output("uname", "microsoft"):
-                with MockCommand.fixed_output("command", "/powershell.exe"):
-                    with MockCommand("powershell.exe") as openCommand:
-                        temp_repo.invoke_extras_command("browse-ci", "origin")
-                        expected_url = create_expected_action_url(git, "github")
-                        openCommand.assert_called(["-NoProfile", "start", expected_url])
+        temp_repo.change_origin_to_github()
+        with modified_env({ "OSTYPE": "linux" }), MockCommand.fixed_output("uname", "microsoft"), \
+            MockCommand.fixed_output("command", "/powershell.exe"), MockCommand("powershell.exe") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("github")
+            openCommand.assert_called(["-NoProfile", "start", expected_url])
 
     def test_browse_gitlab_ci_on_WSL_with_microsoft_key(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, gitlab_origin)
-        with modified_env({ "OSTYPE": "linux" }):
-            with MockCommand.fixed_output("uname", "microsoft"):
-                with MockCommand.fixed_output("command", "/powershell.exe"):
-                    with MockCommand("powershell.exe") as openCommand:
-                        temp_repo.invoke_extras_command("browse-ci", "origin")
-                        expected_url = create_expected_action_url(git, "gitlab")
-                        openCommand.assert_called(["-NoProfile", "start", expected_url])
+        temp_repo.change_origin_to_gitlab()
+        with modified_env({ "OSTYPE": "linux" }), MockCommand.fixed_output("uname", "microsoft"), \
+            MockCommand.fixed_output("command", "/powershell.exe"), MockCommand("powershell.exe") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("gitlab")
+            openCommand.assert_called(["-NoProfile", "start", expected_url])
 
     def test_browse_bitbucket_ci_on_WSL_with_microsoft_key(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, bitbucket_origin)
-        with modified_env({ "OSTYPE": "linux" }):
-            with MockCommand.fixed_output("uname", "microsoft"):
-                with MockCommand.fixed_output("command", "/powershell.exe"):
-                    with MockCommand("powershell.exe") as openCommand:
-                        temp_repo.invoke_extras_command("browse-ci", "origin")
-                        expected_url = create_expected_action_url(git, "bitbucket")
-                        openCommand.assert_called(["-NoProfile", "start", expected_url])
+        temp_repo.change_origin_to_bitbucket()
+        with modified_env({ "OSTYPE": "linux" }), MockCommand.fixed_output("uname", "microsoft"), \
+            MockCommand.fixed_output("command", "/powershell.exe"), MockCommand("powershell.exe") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("bitbucket")
+            openCommand.assert_called(["-NoProfile", "start", expected_url])
 
     def test_browse_github_ci_on_WSL_without_microsoft_key(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, github_origin)
-        with modified_env({ "OSTYPE": "linux" }):
-            with MockCommand.fixed_output("uname", "no-micro-soft"):
-                with MockCommand.fixed_output("command", "/powershell.exe"):
-                    with MockCommand("xdg-open") as openCommand:
-                        temp_repo.invoke_extras_command("browse-ci", "origin")
-                        expected_url = create_expected_action_url(git, "github")
-                        openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_github()
+        with modified_env({ "OSTYPE": "linux" }), MockCommand.fixed_output("uname", "no-micro-soft"), \
+            MockCommand.fixed_output("command", "/powershell.exe"), MockCommand("xdg-open") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("github")
+            openCommand.assert_called([expected_url])
 
     def test_browse_gitlab_ci_on_WSL_without_microsoft_key(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, gitlab_origin)
-        with modified_env({ "OSTYPE": "linux" }):
-            with MockCommand.fixed_output("uname", "no-micro-soft"):
-                with MockCommand.fixed_output("command", "/powershell.exe"):
-                    with MockCommand("xdg-open") as openCommand:
-                        temp_repo.invoke_extras_command("browse-ci", "origin")
-                        expected_url = create_expected_action_url(git, "gitlab")
-                        openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_gitlab()
+        with modified_env({ "OSTYPE": "linux" }), MockCommand.fixed_output("uname", "no-micro-soft"), \
+            MockCommand.fixed_output("command", "/powershell.exe"), MockCommand("xdg-open") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("gitlab")
+            openCommand.assert_called([expected_url])
 
     def test_browse_bitbucket_ci_on_WSL_without_microsoft_key(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, bitbucket_origin)
-        with modified_env({ "OSTYPE": "linux" }):
-            with MockCommand.fixed_output("uname", "no-micro-soft"):
-                with MockCommand.fixed_output("command", "/powershell.exe"):
-                    with MockCommand("xdg-open") as openCommand:
-                        temp_repo.invoke_extras_command("browse-ci", "origin")
-                        expected_url = create_expected_action_url(git, "bitbucket")
-                        openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_bitbucket()
+        with modified_env({ "OSTYPE": "linux" }), MockCommand.fixed_output("uname", "no-micro-soft"), \
+            MockCommand.fixed_output("command", "/powershell.exe"), MockCommand("xdg-open") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("bitbucket")
+            openCommand.assert_called([expected_url])
 
     def test_browse_github_ci_not_mac_or_msys_or_linux(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, github_origin)
-        with modified_env({ "OSTYPE": "unique-system" }):
-            with MockCommand("xdg-open") as openCommand:
-                temp_repo.invoke_extras_command("browse-ci", "origin")
-                expected_url = create_expected_action_url(git, "github")
-                openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_github()
+        with modified_env({ "OSTYPE": "unique-system" }), MockCommand("xdg-open") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("github")
+            openCommand.assert_called([expected_url])
 
     def test_browse_gitlab_ci_not_mac_or_msys_or_linux(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, gitlab_origin)
-        with modified_env({ "OSTYPE": "unique-system" }):
-            with MockCommand("xdg-open") as openCommand:
-                temp_repo.invoke_extras_command("browse-ci", "origin")
-                expected_url = create_expected_action_url(git, "gitlab")
-                openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_gitlab()
+        with modified_env({ "OSTYPE": "unique-system" }), MockCommand("xdg-open") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("gitlab")
+            openCommand.assert_called([expected_url])
 
     def test_browse_bitbucket_ci_not_mac_or_msys_or_linux(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, bitbucket_origin)
-        with modified_env({ "OSTYPE": "unique-system" }):
-            with MockCommand("xdg-open") as openCommand:
-                temp_repo.invoke_extras_command("browse-ci", "origin")
-                expected_url = create_expected_action_url(git, "bitbucket")
-                openCommand.assert_called([expected_url])
+        temp_repo.change_origin_to_bitbucket()
+        with modified_env({ "OSTYPE": "unique-system" }), MockCommand("xdg-open") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            expected_url = get_ci_uri_by_domain("bitbucket")
+            openCommand.assert_called([expected_url])
 
     def test_browse_unknown_site_file(self, temp_repo):
-        git = temp_repo.get_repo_git()
-        set_origin_url(git, unknown_site_origin)
-        with modified_env({ "OSTYPE": "unique-system" }):
-            with MockCommand("xdg-open") as openCommand:
-                temp_repo.invoke_extras_command("browse-ci", "origin")
-                openCommand.assert_called([''])
+        temp_repo.change_origin(UNKNOWN_SITE_ORIGIN)
+        with modified_env({ "OSTYPE": "unique-system" }), MockCommand("xdg-open") as openCommand:
+            temp_repo.invoke_extras_command("browse-ci", "origin")
+            openCommand.assert_called([''])
