cab7bd99d76e7a7f63ce65c105277dd6e10b6fc2
oikarinen <tapio@autolinkki.fi>
2024-11-22 21:32:00 +0200

    fix: format with ruff

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,14 @@
 import pytest
 from helper import TempRepository
 
-def create_repo(dirname = None):
+
+def create_repo(dirname=None):
     repo = TempRepository(dirname)
     tmp_file_a = repo.create_tmp_file()
     tmp_file_b = repo.create_tmp_file()
     repo.switch_cwd_under_repo()
     return repo
+
 
 def init_repo_git_status(repo):
     git = repo.get_repo_git()
@@ -18,11 +20,13 @@ def init_repo_git_status(repo):
     git.config("--local", "user.email", "test@git-extras.com")
     git.commit("-m", "chore: initial commit")
 
+
 @pytest.fixture(scope="module")
 def temp_repo():
     repo = create_repo()
     init_repo_git_status(repo)
     return repo
+
 
 @pytest.fixture(scope="module")
 def named_temp_repo(request):
